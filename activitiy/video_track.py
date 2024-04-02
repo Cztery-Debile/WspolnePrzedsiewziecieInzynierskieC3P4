543fb6f152695721dd0326505734dd0a75886445
gigamichal <115494036+gigamichal@users.noreply.github.com>
2024-04-02 10:38:38 +0200

    Poprawienie twarz(konflikt xd?)

--- activitiy/video_track.py
+++ activitiy/video_track.py
@@ -1,23 +1,43 @@
+<<<<<<< Updated upstream
 import random
+=======
+import os.path
+import random
+import sys
+import threading
+import time
+>>>>>>> Stashed changes
 from collections import defaultdict
 import cv2
 from ultralytics import YOLO
 from src.detection_keypoint import DetectKeypoint
 from src.classification_keypoint import KeypointClassification
+<<<<<<< Updated upstream
+=======
+from  face_detection.compare import get_names_list  # Importujemy funkcję z pliku compare
+sys.path.append(os.path.abspath(r"C:\Users\miszel\Desktop\WspolnePrzedsiewziecieInzynierskieC3P4-activity-facedetection"))
+>>>>>>> Stashed changes
 
 # load yolov8 model
 model_yolo = YOLO('../models/best_today.pt')
 detection_keypoint = DetectKeypoint()
 classification_keypoint = KeypointClassification('czynnosci_latest.pt')
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 center_dict = defaultdict(list)
 object_id_list = []
 color_dict = {}
 max_points = 20  # maksymalna liczba punktów śledzenia
 
 # load video
+<<<<<<< Updated upstream
 video_path = 'x.mp4'
+=======
+video_path = 'Projekt M.mp4'
+>>>>>>> Stashed changes
 cap = cv2.VideoCapture(video_path)
 
 ret = True
@@ -25,6 +45,7 @@ ret = True
 def random_color():
     return tuple(int(random.random() * 255) for _ in range(3))
 
+<<<<<<< Updated upstream
 frame_count = 0
 classifications_dict = {}
 persisted_classifications = {}
@@ -100,3 +121,119 @@ while ret:
 
 cap.release()
 cv2.destroyAllWindows()
+=======
+# Zmienna do śledzenia liczby klatek
+frame_count = 0
+dymy = False
+# Pobranie listy imion i identyfikatorów twarzy
+names_list = get_names_list()
+
+# Funkcja papcer wykonywana przez pierwszy wątek
+def papcer():
+    global ret, frame_count, names_list, dymy
+    classifications_dict = {}
+    persisted_classifications = {}
+    skip_frames = 30
+    while ret:
+        ret, frame = cap.read()
+        if ret:
+            frame_count += 1
+
+            results = model_yolo.track(frame, persist=True)
+            for result in results:
+                boxes = result.boxes.cpu().numpy()
+                ids = result.boxes.id.cpu().numpy().astype(int)
+                for box, id in zip(boxes, ids):
+                    class_id = int(box.cls[0])
+
+                    if class_id == 0:
+                        r = box.xyxy[0].astype(int)
+                        x_min, y_min, x_max, y_max = r
+                        head_region1 = frame[y_min:y_max, x_min:x_max]
+                        for name, face_id in names_list:
+                            if face_id == id:
+                                cv2.putText(frame,
+                                            #f'Name: {face_id}',
+                                             name,
+                                            (x_min, y_min-30),
+                                            cv2.FONT_HERSHEY_SIMPLEX,
+                                            0.5, (0, 0, 255),
+                                            thickness=2
+                                            )
+
+                        if frame_count % 10 == 0 and frame_count % 20 != 0:
+                            #head_region=cv2.cvtColor(head_region1, cv2.COLOR_RGB2BGR)
+                            cv2.imwrite(f"../face_detection/compare/face_{id}.png", head_region1)
+
+                    if class_id == 2:  # person
+                        if frame_count % skip_frames == 0:
+                            r = box.xyxy[0].astype(int)
+                            x_min, y_min, x_max, y_max = r
+
+                            person_region = frame[y_min:y_max, x_min:x_max]
+                            results = detection_keypoint(person_region)
+
+                            if results:
+                                results_keypoint = detection_keypoint.get_xy_keypoint(results)
+                                input_classification = results_keypoint[10:]
+                                results_classification = classification_keypoint(input_classification)
+
+                                classifications_dict[id] = results_classification
+
+                            if id in classifications_dict and frame_count % 2 == 0:
+                                classification_text = classifications_dict[id]
+                                persisted_classifications[id] = classification_text
+
+            for id, classification_text in persisted_classifications.items():
+                if id in classifications_dict:
+                    try:
+                        index = ids.tolist().index(id)
+                        r = boxes[index].xyxy[0].astype(int)
+                        x_min, y_min = r[0], r[1]
+                        (w, h), _ = cv2.getTextSize(
+                            classification_text.upper(),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2
+                        )
+                        text_x = x_min
+                        text_y = y_min - 4
+
+                        if text_y - h < 0:
+                            text_y = y_min + h + 4
+
+                        cv2.putText(frame,
+                                    f'{classification_text.upper()}',
+                                    (text_x, text_y),
+                                    cv2.FONT_HERSHEY_SIMPLEX,
+                                    0.5, (0,0 , 255),
+                                    thickness=2
+                                    )
+                    except ValueError:
+                        print(f"ID {id} not found in ids list. Skipping processing for this id.")
+
+            cv2.imshow('Frame', frame)
+
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+
+            if frame_count % 10 == 0:
+                t2 = threading.Thread(target=compare)
+                t2.start()
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+# Funkcja compare wykonywana przez drugi wątek
+def compare():
+    global names_list
+    names_list = get_names_list()
+    print(names_list)
+
+# Tworzenie wątku dla funkcji papcer
+t1 = threading.Thread(target=papcer)
+
+# Uruchomienie wątku
+t1.start()
+
+# Oczekiwanie na zakończenie wątku
+t1.join()
+>>>>>>> Stashed changes
--- face_detection/compare.py
+++ face_detection/compare.py
@@ -1,29 +1,55 @@
 import numpy as np
 from imgbeddings import imgbeddings
 from PIL import Image as Image1
-from IPython.display import Image, display as IPImage,display
 import psycopg2
 import os
 import ast
 
-conn = psycopg2.connect("postgres://avnadmin:AVNS_bfFc4wTNrJeCe8hQ3Yo@pg-29ab6d03-facesxd.a.aivencloud.com:12523/defaultdb?sslmode=require")
-#tu wrzucić z poprzedniego modelu efekt koncowy
-file_name = "./compare/1.png"  # replace <INSERT YOUR FACE FILE NAME> with the path to your image
-# opening the image
-img =Image1.open(file_name)
-# loading the `imgbeddings`
-ibed = imgbeddings()
-# calculating the embeddings
-embedding = ibed.to_embeddings(img)
 
+def get_names_list():
+    conn = psycopg2.connect(
+        "postgres://avnadmin:AVNS_bfFc4wTNrJeCe8hQ3Yo@pg-29ab6d03-facesxd.a.aivencloud.com:12523/defaultdb?sslmode=require")
 
-cur = conn.cursor()
-string_representation = "["+ ",".join(str(x) for x in embedding[0].tolist()) +"]"
-cur.execute("SELECT * FROM pictures ORDER BY embedding <-> %s LIMIT 1;", (string_representation,))
-rows = cur.fetchall()
-for row in rows:
-    embedded_image = np.array(ast.literal_eval(row[1]))  # Osadzenie z bazy danych
-    norm_distance = np.linalg.norm(embedded_image - embedding[0])  # Odległość między osadzeniami
-    print("Odległość między osadzeniami:", norm_distance)
-    print(row[2])
-cur.close()
+    # Ścieżka do katalogu zawierającego zdjęcia do porównania
+    compare_folder = "../face_detection/compare"
+
+    # Inicjalizacja pustej listy na imiona
+    names_list = []
+
+    # Przetwórz każde zdjęcie w katalogu porównawczym
+    for filename in os.listdir(compare_folder):
+        if filename.startswith("face_") and filename.endswith(".png"):
+            # Pobierz ID twarzy z nazwy pliku
+            face_id = int(filename.split("_")[1].split(".")[0])
+
+            file_path = os.path.join(compare_folder, filename)
+            img = Image1.open(file_path)
+            ibed = imgbeddings()
+            embedding = ibed.to_embeddings(img)
+
+            cur = conn.cursor()
+            string_representation = "[" + ",".join(str(x) for x in embedding[0].tolist()) + "]"
+            cur.execute("SELECT * FROM pictures ORDER BY embedding <-> %s LIMIT 1;", (string_representation,))
+            rows = cur.fetchall()
+            for row in rows:
+                embedded_image = np.array(ast.literal_eval(row[1]))
+                norm_distance = np.linalg.norm(embedded_image - embedding[0])
+                name = row[2]
+                print(norm_distance)
+                # Dodaj nowe imię i ID do listy
+                if norm_distance < 10.2:
+                    # Sprawdź, czy na liście nie ma już tej samej nazwy
+                    if all(name != existing_name for existing_name, _ in names_list):
+                        # Jeśli nazwa nie istnieje na liście, dodaj nową parę (name, face_id)
+                        new_entry = (name, face_id)
+                        names_list.append(new_entry)
+                    else:
+                        # Jeśli nazwa już istnieje na liście, wykonaj odpowiednie działania (np. zignoruj)
+                        pass
+            cur.close()
+
+    conn.close()
+    print(names_list)
+    return  names_list
+
+get_names_list()
